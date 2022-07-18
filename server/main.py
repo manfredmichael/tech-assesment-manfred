25b040cf0749655027209f586ec6f9d8c88d616a
manfred michael <koskosanrider@gmail.com>
2022-07-18 08:21:59 +0700

    Add heatmap endpoint

--- server/main.py
+++ server/main.py
@@ -5,14 +5,23 @@ from app import app
 from flask import Flask, request 
 import json
 
-@app.route("/predicts/", methods=['POST'])
-def submit_file():
+@app.route("/predict", methods=['POST'])
+def make_prediction():
     if request.method == 'POST':
         file = request.files['file']
         annotations = json.load(request.files['data'])['annotations']
         image = Image.open(BytesIO(file.read()))
         count = predict(image, annotations)
         return {'count': count} 
+
+@app.route("/heatmap", methods=['POST'])
+def evaluate_heatmap():
+    if request.method == 'POST':
+        file = request.files['file']
+        annotations = json.load(request.files['data'])['annotations']
+        image = Image.open(BytesIO(file.read()))
+        count, heatmap = predict(image, annotations, return_density_map)
+        return {'count': count, 'heatmap': heatmap} 
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
