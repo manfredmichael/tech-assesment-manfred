cacb8db172e1b680cc6b85d388fabf92be6f5e7e
manfred michael <koskosanrider@gmail.com>
2022-07-20 12:05:09 +0700

    Convert uploaded image to RGB

--- streamlit/app.py
+++ streamlit/app.py
@@ -51,11 +51,15 @@ def color_annotation_app():
     #
     """
     )
+    
+    try:
+        bg_image = Image.open("img/annotation.jpeg")
+    except:
+        bg_image = None 
 
-    bg_image = Image.open("img/annotation.jpeg")
     image_file = st.file_uploader('Upload your image', type=['jpg', 'jpeg', 'png'], accept_multiple_files=False)
     if image_file is not None: 
-        bg_image = Image.open(image_file)
+        bg_image = Image.open(image_file).convert('RGB')
         bg_image.save('img/annotation.jpeg')
 
     label_color = (
